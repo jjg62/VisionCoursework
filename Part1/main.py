98e3aaabd7ae9f41fd3a85c815c462ab26c8a790
Jac <jjg62@bath.ac.uk>
2022-02-18 15:29:04 +0000

    Ducks now appear

--- Part1/main.py
+++ Part1/main.py
@@ -1,1 +1,10 @@
-print("Hello World");
+import matplotlib, cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Load in and convert image
+pic = cv2.cvtColor(cv2.imread('ducks.jpeg'), cv2.COLOR_BGR2GRAY)
+small_pic = pic[1200:1800, 1200:1800]
+
+plt.imshow(cv2.cvtColor(small_pic, cv2.COLOR_GRAY2RGB))
+plt.show()
