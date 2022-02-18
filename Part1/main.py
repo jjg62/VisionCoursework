65be9bb66bf47808dc7f7d4afc810970d255e4df
Jac <jjg62@bath.ac.uk>
2022-02-18 16:10:07 +0000

    Edge detection

--- Part1/main.py
+++ Part1/main.py
@@ -3,16 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Load in and convert image
-pic = cv2.cvtColor(cv2.imread('ducks.jpeg'), cv2.COLOR_BGR2GRAY)
-small_pic = pic[1200:1800, 1200:1800]
+pic = cv2.cvtColor(cv2.imread('angle/image1.png'), cv2.COLOR_BGR2GRAY)
 
-plt.imshow(cv2.cvtColor(small_pic, cv2.COLOR_GRAY2RGB))
+
+#Blur image to reduce effect of noise
+pic = cv2.GaussianBlur(pic, (9,9), 2)
+
+#Find position of edge pixels
+pic = cv2.Canny(pic, threshold1=70, threshold2=110)
+
+#Fit line
+
+plt.imshow(cv2.cvtColor(pic, cv2.COLOR_GRAY2RGB))
 plt.show()
-
-
-
-# Use filter2D to get gradients after blurring
-# Store magnitudes and angles of gradient
-# Non-maximum surpression - make the edges thinner
-# Connect edgse by fitting lines
-# Use fitted lines to work out angles
