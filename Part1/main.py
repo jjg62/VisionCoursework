8c01fec35a8d22e6690f52610f744cba90c8e29a
Jac <jjg62@bath.ac.uk>
2022-02-18 16:22:57 +0000

    np.where

--- Part1/main.py
+++ Part1/main.py
@@ -5,14 +5,23 @@ import matplotlib.pyplot as plt
 #Load in and convert image
 pic = cv2.cvtColor(cv2.imread('angle/image1.png'), cv2.COLOR_BGR2GRAY)
 
-
 #Blur image to reduce effect of noise
-pic = cv2.GaussianBlur(pic, (9,9), 2)
+blurPic = cv2.GaussianBlur(pic, (9,9), 2)
 
 #Find position of edge pixels
-pic = cv2.Canny(pic, threshold1=70, threshold2=110)
+edges = cv2.Canny(blurPic, threshold1=70, threshold2=110)
 
 #Fit line
+edgePixelLocations = np.transpose(np.array(np.where(edges != 0)))
 
-plt.imshow(cv2.cvtColor(pic, cv2.COLOR_GRAY2RGB))
-plt.show()
+print(edgePixelLocations)
+for point in edgePixelLocations:
+    #go through every line
+    #test distance
+    #vote for one(s?) with smallest distance
+
+
+#def distanceFromLine(m, c, )
+
+#plt.imshow(cv2.cvtColor(pic, cv2.COLOR_GRAY2RGB))
+#plt.show()
