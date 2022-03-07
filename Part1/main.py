56ca372221cb021f42ff1f79b9ff91a85ce93cca
Jac <jjg62@bath.ac.uk>
2022-03-07 20:11:04 +0000

    Final (?) version of Task 1, gives accurate results as shown in the document

--- Part1/main.py
+++ Part1/main.py
@@ -1,5 +1,4 @@
 import math
-
 import matplotlib, cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,24 +6,26 @@ import matplotlib.pyplot as plt
 
 
 #Load in and convert image
-pic = cv2.cvtColor(cv2.imread('angle/image4.png'), cv2.COLOR_BGR2GRAY)
+pic = cv2.cvtColor(cv2.imread('angle/image7.png'), cv2.COLOR_BGR2GRAY)
 
-plt.subplot(141), plt.imshow(cv2.cvtColor(pic, cv2.COLOR_GRAY2RGB)), plt.title('Original')
+plt.subplot(221), plt.imshow(cv2.cvtColor(pic, cv2.COLOR_GRAY2RGB)), plt.title('Original')
 
 #Blur image to reduce effect of noise
-#blurPic = cv2.GaussianBlur(pic, (9,9), 2)
+blurPic = cv2.GaussianBlur(pic, (9,9), 2)
 
 #Find position of edge pixels
 edges = cv2.Canny(pic, threshold1=100, threshold2=0)
 
+#Plot Results of Edge Detection
+plt.subplot(222), plt.imshow(cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)), plt.title('Canny Edge Detection')
 
-plt.subplot(142), plt.imshow(cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)), plt.title('Canny Edge Detection')
-
-
+#Find co-ordinates of points detected
 edgePixelLocations = np.transpose(np.array(np.where(edges > 0)))
 
+#Get a maximum rho size by taking the diagonal size of image
 imgDiagSize = math.sqrt(pic.shape[0]**2 + pic.shape[1]**2)
 
+#Resolutions of accumulator
 accResolutionRho = 1000
 accResolutionTheta = 180
 accumulator = np.zeros((accResolutionTheta, accResolutionRho))
@@ -32,8 +33,8 @@ accumulator = np.zeros((accResolutionTheta, accResolutionRho))
 bestLine1 = (0, 0)
 bestLine2 = (0, 0)
 
-
-def testLines():
+#Find two best fitting lines from Hough Transform
+def houghLines():
     global bestLine1, bestLine2
     for point in edgePixelLocations:
         #Iterate over angles from 0 to pi
@@ -47,17 +48,22 @@ def testLines():
             j = accResolutionRho * (rho + imgDiagSize) / (2 * imgDiagSize)
             j = round(j)
 
+            #Add a vote to the accumulator
             accumulator[i, j] += 1
 
-    plt.subplot(143), plt.imshow(accumulator), plt.title('Rho-Theta Hough Space'), plt.xlabel("Rho Index"), plt.ylabel("Theta Index")
+    #Plot the Hough space
+    plt.subplot(223), plt.imshow(accumulator), plt.title('Rho-Theta Hough Space'), plt.xlabel("Rho Index"), plt.ylabel("Theta Index")
+
+    #Get co-ordinates of best line
     bestLine1 = np.unravel_index(accumulator.argmax(), accumulator.shape)
 
-    #Eliminate that line and its neighbours within n?
-    n = 40
+    #Eliminate that line and its neighbouring n//2 rows of co-ordinate pairs (such that 2 similar angles are not picked)
+    n = 9
     for k in range(n):
         for m in range(n):
-            accumulator[bestLine1[0] - n//2 + k, bestLine1[1] - n//2 + m] = 0
+            accumulator[bestLine1[0] - n//2 + k] = 0
 
+    #Now get the maximum again for second line
     bestLine2 = np.unravel_index(accumulator.argmax(), accumulator.shape)
 
 
@@ -70,6 +76,8 @@ def findIntersection():
 
     rho1 = (bestLine1[1] * 2 * imgDiagSize / accResolutionRho) - imgDiagSize
     rho2 = (bestLine2[1] * 2 * imgDiagSize / accResolutionRho) - imgDiagSize
+
+    #Check exceptions first - if each line is vertical/horizontal
 
     if theta1 == 0: #Angle is 0, vertical
         if theta2 == 0: return (-1, -1) #Error - shouldn't have parallel lines
@@ -100,7 +108,7 @@ def findIntersection():
                 x = (rho1 - y * math.sin(theta1)) / math.cos(theta1)
 
         else:
-            #Neither of the lines are exceptions
+            #Neither of the lines are exceptions, can solve a simulatneous equation for intersection
             x = (rho2 * math.sin(theta1) - rho1 * math.sin(theta2)) / (math.cos(theta2) * math.sin(theta1) - math.cos(theta1) * math.sin(theta2))
             y = (rho1 - x * math.cos(theta1)) / math.sin(theta1)
 
@@ -125,6 +133,7 @@ def squareDistanceFromLine(theta, rho, pointX, pointY):
         x2 = 10
         y2 = (rho - x2 * math.cos(theta)) / math.sin(theta)
 
+        #Calculate distance using a vector made from the two points
         distSqr = (((x2 - x1)*(y1 - pointY) - (x1 - pointX)*(y2 - y1))**2)/((x2-x1)**2 + (y2-y1)**2)
 
         return distSqr
@@ -146,11 +155,10 @@ def ensureCorrectAngle(foundAngle):
     p2 = None
 
     lineDistThreshold = 2
-    intersectionDistThreshold = 120
     maxDistLine1 = -1
     maxDistLine2 = -1
 
-    #Find two points - one on the first line, one on the second - that are a suitable distance away from intersection
+    #Find two points - one on the first line, one on the second - that are as far away from intersection as possible
     for edge in edgePixelLocations:
         dist = (edge[1] - p0[0]) ** 2 + (edge[0] - p0[1]) ** 2
         if squareDistanceFromLine(theta1, rho1, edge[1], edge[0]) < lineDistThreshold**2:
@@ -175,10 +183,9 @@ def ensureCorrectAngle(foundAngle):
         return otherAngle
 
 
-
+#Draw lines on a plot, from their rho and theta parameters
 def drawLine(img, theta, rho):
     # If line is vertical/horizontal, exception cases
-
     if theta == math.pi/2:
         x1 = 0
         y1 = rho
@@ -215,23 +222,24 @@ def drawLine(img, theta, rho):
 
     cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 4)
 
-testLines()
+#Find 2 Best fitting lines, save them to bestLine1 and bestLine2
+houghLines()
 
-print(bestLine1)
-print(bestLine2)
-
+#Find angle between the two theta parameters
 angle = math.fabs(bestLine1[0] - bestLine2[0]) * 180/accResolutionTheta
-print("ANGLE: ", angle)
 
+#Copy the Canny image to draw the lines on
 img = edges[:]
 
+#Draw the two found lines onto img
 drawLine(img, bestLine1[0] * math.pi / accResolutionTheta, (bestLine1[1] - accResolutionRho // 2) * 2 * imgDiagSize / accResolutionRho)
 drawLine(img, bestLine2[0] * math.pi / accResolutionTheta, (bestLine2[1] - accResolutionRho // 2) * 2 * imgDiagSize / accResolutionRho)
 
+#Use ensureCorrectedAngle to make sure the angle is the BETWEEN the lines on the image, not 180 - it
+print("ANGLE:", ensureCorrectAngle(angle))
 
-print("CORRECTED ANGLE:", ensureCorrectAngle(angle))
-
-plt.subplot(144), plt.imshow(img), plt.title('Redrawn Lines')
+#Show the drawn lines
+plt.subplot(224), plt.imshow(img), plt.title('Redrawn Lines')
 
 plt.show()
 
