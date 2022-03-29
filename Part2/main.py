ebb43849e1ed809ff35b7dd83bb62eadc0809a1f
Jac <jjg62@bath.ac.uk>
2022-03-29 23:25:36 +0000

    Scaling + other improvements

--- Part2/main.py
+++ Part2/main.py
@@ -6,21 +6,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 smallestTemplateSize = 16
+largestTemplateSize = 512
 numberOfSizes = 8
 numberOfRotations = 8
 gaussianKSize = 7
+pixelInterval = 4
+
+correlationThreshold = 0.85
 
 #For now, defualt image size (512x512) is the largest template size
 
 
 def normalisedCrossCorrelation(template, test):
+
     testDimensions = test.shape
     templateDimensions = template.shape
 
     normalisedTemplate = (template - template.mean()) / template.std()
 
-    for y1 in range(0, testDimensions[1] - templateDimensions[1]):
-        for x1 in range(0, testDimensions[0] - templateDimensions[0]):
+    maxCorrelation = -999999999
+    maxPosition = (0, 0)
+
+    for y1 in range(0, testDimensions[1] - templateDimensions[1], pixelInterval):
+        for x1 in range(0, testDimensions[0] - templateDimensions[0], pixelInterval):
             #Get subimage of test
             testSub = test[y1:y1+templateDimensions[1], x1:(x1+templateDimensions[0])]
 
@@ -29,16 +37,24 @@ def normalisedCrossCorrelation(template, test):
             #print(testSub.std())
 
             #Check correlation
-            correlation = np.dot(normalisedTestSub.flatten(), normalisedTemplate.flatten())
-            #if correlation > 1200:
-                #plt.imshow(cv.cvtColor(testSub, cv.COLOR_GRAY2RGB))
-                #plt.show()
+            correlation = np.dot(normalisedTestSub.flatten(), normalisedTemplate.flatten()) / (templateDimensions[0] * templateDimensions[1])
+            if(correlation > maxCorrelation):
+                maxCorrelation = correlation
+                maxPosition = (x1, y1)
+
+    print(maxCorrelation)
+    plt.imshow(test[maxPosition[1]:maxPosition[1] + templateDimensions[1],
+               maxPosition[0]:maxPosition[0] + templateDimensions[0]])
+    plt.show()
+
+    return maxCorrelation
+
 
 
 imageNames = listdir('training')
 #for imageName in imageNames:
 
-imageName = '011-trash.png'
+imageName = '016-house.png'
 
 #Read image file and convert
 temp = cv.imread('training/' + imageName)
@@ -47,18 +63,25 @@ templateGray = cv.cvtColor(temp, cv.COLOR_BGR2GRAY)
 #Remove white background
 templateGray[np.where(templateGray > 250)] = 0
 
-#Scale it down - first apply Gaussian Blur
-templateGray = cv2.GaussianBlur(templateGray, (gaussianKSize, gaussianKSize), 1)
-templateGray = cv2.resize(templateGray, (50, 50))
-#Later make it so many different sizes are used, for now just 1
-
-#Slide/Convolve over image
+#Read test img
 testImg = cv.cvtColor(cv.imread('img.png'), cv.COLOR_BGR2GRAY)
-testImg[np.where(testImg > 250)] = 0
-plt.imshow(testImg)
+testImg[np.where(testImg > 250)] = 0 #Set background to black
+
+plt.imshow(cv.cvtColor(templateGray, cv.COLOR_GRAY2RGB));
 plt.show()
 
-normalisedCrossCorrelation(templateGray, testImg)
+for i in range(numberOfSizes, -1, -1):
+
+    sizeInterval = (largestTemplateSize - smallestTemplateSize) // numberOfSizes
+    currentSize = smallestTemplateSize + sizeInterval * i
+
+    print(currentSize)
+
+    templateGray = cv2.GaussianBlur(templateGray, (gaussianKSize, gaussianKSize), 1)
+    templateGray = cv2.resize(templateGray, (currentSize, currentSize))
+
+    normalisedCrossCorrelation(templateGray, testImg)
+
 
 
 
