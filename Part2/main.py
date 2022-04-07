8ca8e1f2e89674b417bbe41ff01bbdcf637658f4
jjg62 <jjg62@bath.ac.uk>
2022-04-07 22:16:07 +0000

    Automated Tests, but template sizes needs work!

--- Part2/main.py
+++ Part2/main.py
@@ -1,25 +1,32 @@
+import math
 from os import listdir
 
 import cv2
 import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.signal
+
 from match import Match
 
-smallestTemplateSize = 16
+
+multiResolutionRatio = 2
+multiResolutionLayers = 3
+multiResolutionThresholds = [0.9, 0.85, 0.7]
+
+smallestTemplateSize = 32
 largestTemplateSize = 512
 templateShrinkRatio = 2
 
+templateGaussianKSize = 5
+templateGaussianStd = templateShrinkRatio / math.pi
 
-numberOfSizes = 8
-numberOfRotations = 8
+testGaussianKSize = 5
+testGaussianStd = multiResolutionRatio / math.pi
 
+pixelStride = 1
 
-gaussianKSize = 5
-gaussianStd = 1
-pixelInterval = 1
-
-correlationThreshold = 0.625
+imageNames = listdir('training')
 
 #For now, default image size (512x512) is the largest template size
 
@@ -43,74 +50,57 @@ def convolveNCC(template, test, instanceName):
     testDimensions = (test.shape[1], test.shape[0])
     templateDimensions = (template.shape[1], template.shape[0])
 
+    DEBUG_NCC_MAP = np.zeros((test.shape[0] - template.shape[0], test.shape[1] - template.shape[1]))
+
     normalisedTemplate = (template - template.mean()) / template.std()
 
     maxCorrelation = -2
     bestMatch = None
 
-    for y in range(0, testDimensions[1] - templateDimensions[1], pixelInterval):
-        for x in range(0, testDimensions[0] - templateDimensions[0], pixelInterval):
+    for y in range(0, testDimensions[1] - templateDimensions[1], pixelStride):
+        for x in range(0, testDimensions[0] - templateDimensions[0], pixelStride):
 
             correlation = normalisedCrossCorrelation(normalisedTemplate, test, x, y)
+
+            DEBUG_NCC_MAP[y, x] = correlation
 
             if correlation > maxCorrelation:
                 bestMatch = Match((x, y), templateDimensions, instanceName, correlation)
                 maxCorrelation = correlation
 
+
     return bestMatch
 
 
-imageNames = listdir('training')
-
-allGoodMatches = []
-
-#Read test img
-testImg = cv.cvtColor(cv.imread('test_image_17.png'), cv.COLOR_BGR2GRAY)
-#testImg[np.where(testImg > 250)] = 0 #Set background to black
-
-multiResolutionRatio = 2
-multiResolutionLayers = 3
-multiResolutionThresholds = [0.85, 0.75, 0.7]
-
-#Create arrays with images at each resolution
-#Starting with biggest (actual size), shrinking by 2 each time
-testImageMultiResolution = []
-
-for layer in range(multiResolutionLayers):
-    testImageMultiResolution.append(testImg)
-
-    testImg = cv2.GaussianBlur(testImg, (gaussianKSize, gaussianKSize), gaussianStd)
-
-    #Divide size by multiResolutionRation
-    nextSize = (testImg.shape[0] // multiResolutionRatio, testImg.shape[1] // multiResolutionRatio)
-    testImg = cv2.resize(testImg, nextSize)
-
-
-def findBestMatch(layer, topLeft, bottomRight, size):
+def findBestMatch(layer, topLeft, bottomRight, size, imageName):
     global testImageMultiResolution, templateImageMultiResolution, multiResolutionThresholds, templateShrinkRatio
+
 
     # Get test and template image for this resolution
     test = testImageMultiResolution[layer]
     template = templateImageMultiResolution[layer]
     threshold = multiResolutionThresholds[layer]
 
-    #plt.subplot(131), plt.imshow(test)
+
 
     #Crop test image to area we know template resides from previous call
     test = test[topLeft[1]:bottomRight[1], topLeft[0]:bottomRight[0]]
 
-    #plt.subplot(132), plt.imshow(test)
+    #plt.subplot(131), plt.imshow(test)
+    #plt.subplot(132), plt.imshow(template)
 
-    currentSize = largestTemplateSize
+
+    currentSize = largestTemplateSize // multiResolutionRatio ** layer
 
     bestMatch = None
     bestCorrelation = -2
 
-    while currentSize >= smallestTemplateSize:
+    templateUsed = None
+    while currentSize >= smallestTemplateSize // multiResolutionRatio ** layer:
 
         #If not on the smallest resolution (we know the size), skip every size except the one we want
 
-        if size is None or size == currentSize:
+        if size is None or abs(size - currentSize) < 6: #don't know how to fix this???
 
             bestMatchAtThisSize = convolveNCC(template, test, imageName)
 
@@ -118,69 +108,102 @@ def findBestMatch(layer, topLeft, bottomRight, size):
                 bestMatch = bestMatchAtThisSize
                 bestCorrelation = bestMatch.correlation
 
-        currentSize = currentSize // templateShrinkRatio
+                templateUsed = template[:]
 
-        template = cv2.GaussianBlur(template, (gaussianKSize, gaussianKSize), gaussianStd)
-        template = cv2.resize(template, (currentSize, currentSize))
+        currentSize = int(currentSize // templateShrinkRatio)
 
-    if bestMatch.correlation > threshold:
+        template = cv2.GaussianBlur(template, (templateGaussianKSize, templateGaussianKSize), templateGaussianStd)
+        template = cv2.resize(template, (currentSize, currentSize), interpolation=cv.INTER_NEAREST)
+
+    #plt.subplot(133), plt.imshow(templateUsed)
+    #plt.show()
+
+    if bestMatch is not None and bestMatch.correlation > threshold:
         if layer == 0:
             #Match was found on a sub-image, need to get co-ordinates on whole image
             bestMatch.pos = (bestMatch.pos[0] + topLeft[0], bestMatch.pos[1] + topLeft[1])
             return bestMatch
         else:
-            newTopLeft = ((topLeft[0] + bestMatch.pos[0]-1)*multiResolutionRatio, (topLeft[1] + bestMatch.pos[1]-1)*multiResolutionRatio)
-            newBottomRight = (newTopLeft[0] + (bestMatch.size[0] + 2)*multiResolutionRatio, newTopLeft[1] + (bestMatch.size[1] + 2)*multiResolutionRatio)
+            newTopLeft = ((topLeft[0] + bestMatch.pos[0]-4)*multiResolutionRatio, (topLeft[1] + bestMatch.pos[1]-4)*multiResolutionRatio)
+            newBottomRight = (newTopLeft[0] + (bestMatch.size[0] + 8)*multiResolutionRatio, newTopLeft[1] + (bestMatch.size[1] + 8)*multiResolutionRatio)
 
-            return findBestMatch(layer-1, newTopLeft, newBottomRight, bestMatch.size[0]*multiResolutionRatio)
+            return findBestMatch(layer-1, newTopLeft, newBottomRight, bestMatch.size[0]*multiResolutionRatio, imageName)
 
     #print("FAILED TO GET MATCH AT LAYER", layer, "CORRELATION:", bestMatch.correlation)
     return None
 
 
+testImageMultiResolution = []
+templateImageMultiResolution = []
 
-# Go to higher resolution
+def recognize(testImageName, showResult):
+
+        global testImageMultiResolution, templateImageMultiResolution
+
+        allGoodMatches = []
+
+        #Read test img
+        testImg = cv.cvtColor(cv.imread("scale_test/"+testImageName), cv.COLOR_BGR2GRAY)
+        testImg[np.where(testImg > 230)] = 0 #Set background to black
+
+        #Create arrays with images at each resolution
+        #Starting with biggest (actual size), shrinking by 2 each time
+
+        testImageMultiResolution = []
+        for layer in range(multiResolutionLayers):
+            testImageMultiResolution.append(testImg)
+
+            testImg = cv2.GaussianBlur(testImg, (testGaussianKSize, testGaussianKSize), testGaussianStd)
+
+            #Divide size by multiResolutionRation
+            nextSize = (testImg.shape[0] // multiResolutionRatio, testImg.shape[1] // multiResolutionRatio)
+            testImg = cv2.resize(testImg, nextSize, cv2.INTER_NEAREST)
 
 
-for imageName in imageNames:
 
-    print(imageName)
-    #Read image file and convert
-    temp = cv.imread('training/' + imageName)
-    templateGray = cv.cvtColor(temp, cv.COLOR_BGR2GRAY)
+        for imageName in imageNames:
 
-    #Remove white background
-    #templateGray[np.where(templateGray > 250)] = 0
+            #print(imageName)
+            #Read image file and convert
+            temp = cv.imread('training/' + imageName)
+            templateGray = cv.cvtColor(temp, cv.COLOR_BGR2GRAY)
 
-    #Generate array of different resolutions of the template image
-    templateImageMultiResolution = []
+            #Remove white background
+            templateGray[np.where(templateGray > 230)] = 0
 
-    for layer in range(multiResolutionLayers):
-        templateImageMultiResolution.append(templateGray)
+            #Generate array of different resolutions of the template image
+            templateImageMultiResolution = []
+            for layer in range(multiResolutionLayers):
+                templateImageMultiResolution.append(templateGray)
 
-        templateGray = cv2.GaussianBlur(templateGray, (gaussianKSize, gaussianKSize), gaussianStd)
+                templateGray = cv2.GaussianBlur(templateGray, (templateGaussianKSize, templateGaussianKSize), templateGaussianStd)
 
-        # Divide size by multiResolutionRation
-        nextSize = (templateGray.shape[0] // multiResolutionRatio, templateGray.shape[1] // multiResolutionRatio)
-        templateGray = cv2.resize(templateGray, nextSize)
+                # Divide size by multiResolutionRation
+                nextSize = (templateGray.shape[0] // multiResolutionRatio, templateGray.shape[1] // multiResolutionRatio)
+                templateGray = cv2.resize(templateGray, nextSize)
 
-    smallestResolutionSize = ((testImageMultiResolution[multiResolutionLayers - 1]).shape[1], (testImageMultiResolution[multiResolutionLayers - 1]).shape[0])
-    bestMatch = findBestMatch(multiResolutionLayers - 1, (0,0), smallestResolutionSize, None)
-    if bestMatch is not None:
-        allGoodMatches.append(bestMatch)
-        print(len(allGoodMatches))
+            smallestResolutionSize = ((testImageMultiResolution[multiResolutionLayers - 1]).shape[1], (testImageMultiResolution[multiResolutionLayers - 1]).shape[0])
+            bestMatch = findBestMatch(multiResolutionLayers - 1, (0,0), smallestResolutionSize, None, imageName)
 
-#Get highest resolution back
-testImg = cv.cvtColor(testImageMultiResolution[0], cv.COLOR_GRAY2BGR)
+            if bestMatch is not None:
+                allGoodMatches.append(bestMatch)
+                print(len(allGoodMatches))
 
-#Draw bounding boxes
-for match in allGoodMatches:
-    testImg = cv.rectangle(testImg, match.pos, (match.pos[0] + match.size[0], match.pos[1] + match.size[1]), (255, 0, 0), 4)
-    cv.putText(testImg, match.instanceName, (match.pos[0], match.pos[1] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
-plt.imshow(testImg)
-plt.show()
+        if showResult:
+            #Get highest resolution back
+            testImg = cv.cvtColor(testImageMultiResolution[0], cv.COLOR_GRAY2BGR)
 
+            #Draw bounding boxes
+            for match in allGoodMatches:
+                testImg = cv.rectangle(testImg, match.pos, (match.pos[0] + match.size[0], match.pos[1] + match.size[1]), (255, 0, 0), 4)
+                cv.putText(testImg, match.instanceName, (match.pos[0], match.pos[1] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+
+            plt.imshow(testImg)
+            plt.show()
+
+        return allGoodMatches
 
 
 
---
+++ Part2/test.py
@@ -0,0 +1,50 @@
+import main
+from os import listdir
+import time
+
+testImageNames = listdir('scale_test')
+
+
+totalFalseNegatives = 0
+totalFalsePositives = 0
+
+for testImageName in testImageNames:
+
+    print(testImageName)
+
+    #Read the text file containing actual labels
+    actualLabels = []
+    f = open("annotations/" + testImageName[:-4] + ".txt") #Open annotations file
+    lines = f.readlines()
+    for line in lines:
+        actualLabels.append(line.partition(",")[0])
+
+    startTime = time.time()
+
+    #Run the program on this test image
+    matches = main.recognize(testImageName, True)
+
+
+    foundLabels = []
+
+    #Find false positives - found labels that weren't in the actual list
+    print("False Positives: ")
+    for match in matches:
+        matchName = match.instanceName[4:-4]
+        foundLabels.append(matchName)
+        if(matchName not in actualLabels):
+            totalFalsePositives += 1
+            print(matchName)
+
+    #Find false negatives: actual labels that weren't found
+    print("False Negatives: ")
+    for actual in actualLabels:
+        if(actual not in foundLabels):
+            totalFalseNegatives += 1
+            print(actual)
+
+    print("TIME TAKEN: ", time.time() - startTime, "\n")
+
+
+print("TOTAL FALSE POSITIVES:", totalFalsePositives)
+print("TOTAL FALSE NEGATIVES:", totalFalseNegatives)
